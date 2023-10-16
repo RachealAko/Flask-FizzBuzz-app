583b8aa53ee196b2ecd9029042c77214c34d1d20
Racheal Ako <rachealako22@gmail.com>
2023-10-16 12:36:43 +0100

    i am done with the task

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-# write your code here
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder="template")
+
+@app.route('/fizzbuzz')
+def fizzbuzz():
+    return render_template('fizzbuzz.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
